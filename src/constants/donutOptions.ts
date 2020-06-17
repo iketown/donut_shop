28832281c45927e787f4462a03fea22f52d34687
ike76 <ike76@me.com>
2020-06-17 08:41:28 -0500

    change getRandomDonuts

--- src/constants/donutOptions.ts
+++ src/constants/donutOptions.ts
@@ -1,25 +1,8 @@
-import shortid from "shortid";
-
 export const donutOptions = {
   frostingColor: ["blue", "pink", "orange", "brown"],
   glazeFlavor: ["chocolate", "strawberry", "vanilla", "none"],
   sprinkles: [true, false],
 };
-
-type attrType = "frostingColor" | "glazeFlavor" | "sprinkles";
-
-const getRandomAttr = (attr: attrType) => {
-  const opts = donutOptions[attr];
-  const index = Math.floor(Math.random() * opts.length);
-  return opts[index];
-};
-
-export const getRandomDonut: () => Donut = () => ({
-  id: shortid.generate(),
-  frostingColor: getRandomAttr("frostingColor") as string,
-  glazeFlavor: getRandomAttr("glazeFlavor") as string,
-  sprinkles: getRandomAttr("sprinkles") as boolean,
-});
 
 export const prices = {
   donut: 1.1,
