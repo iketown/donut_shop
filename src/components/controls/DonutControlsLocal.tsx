cb4d7773eb341f0081db8e1d3a183ab724aa091b
ike76 <ike76@me.com>
2020-06-17 09:44:15 -0500

    frosting select working

--- src/components/controls/DonutControlsLocal.tsx
+++ src/components/controls/DonutControlsLocal.tsx
@@ -4,9 +4,13 @@ import DonutControls from "./DonutControls";
 const DonutControlsLocal = () => {
   const [glazeFlavor, setGlazeFlavor] = useState("");
   const [sprinkles, setSprinkles] = useState(false);
+  const [frostingColor, setFrostingColor] = useState("");
+
   return (
     <DonutControls
       {...{
+        frostingColor,
+        setFrostingColor,
         sprinkles,
         setSprinkles,
         glazeFlavor,
