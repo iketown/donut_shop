93fe8470039c834bb67456f47622fc83f6d25cf9
ike76 <ike76@me.com>
2020-06-17 11:43:07 -0500

    random button works

--- src/components/controls/DonutControlsLocal.tsx
+++ src/components/controls/DonutControlsLocal.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import DonutControls from "./DonutControls";
+import getRandomDonut from "../../helpers/getRandomDonut";
 
 const DonutControlsLocal = () => {
   const [glazeFlavor, setGlazeFlavor] = useState("");
   const [sprinkles, setSprinkles] = useState(false);
   const [frostingColor, setFrostingColor] = useState("");
 
+  const handleRandom = () => {
+    const { frostingColor, glazeFlavor, sprinkles } = getRandomDonut();
+    setGlazeFlavor(glazeFlavor);
+    setSprinkles(sprinkles);
+    setFrostingColor(frostingColor);
+  };
   return (
     <DonutControls
       {...{
+        handleRandom,
         frostingColor,
         setFrostingColor,
         sprinkles,
